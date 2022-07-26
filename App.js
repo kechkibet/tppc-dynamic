d8772c1bad6c4c81dd7693b2444fb614b9235c50
Fred.Kipkemoi <kipkemoifred2018@gmail.com>
2022-07-26 15:24:38 +0300

    shows toast

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text,Button, View,NativeModules } from 'react-native';
+const { CalendarModule } = NativeModules;
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app dsafds!</Text>
       <StatusBar style="auto" />
+      <Button
+     title="Click to invoke your native module!"
+     color="#841584"
+     onPress={onPress}/>
     </View>
+    
   );
 }
+const onPress = () => {
+  CalendarModule.createCalendarEvent('testName', 'testLocation');
+};
 
 const styles = StyleSheet.create({
   container: {
---
+++ android/app/src/main/java/com/myreactnative/CalendarModule.java
@@ -0,0 +1,32 @@
+package com.myreactnative;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import java.util.Map;
+import java.util.HashMap;
+
+public class CalendarModule extends ReactContextBaseJavaModule {
+    CalendarModule(ReactApplicationContext context) {
+        super(context);
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "CalendarModule";
+    }
+
+    @ReactMethod
+    public void createCalendarEvent(String name, String location) {
+        Log.d("CalendarModule", "Create event called with name: " + name
+                + " and location: " + location);
+        Toast. makeText(getReactApplicationContext(),"Hello Javatpoint", Toast. LENGTH_SHORT).show();
+    }
+}
